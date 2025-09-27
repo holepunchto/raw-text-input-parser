3de22312d6f0e923de727d3884e8bd846e8bedb8
Fred <gasolin@gmail.com>
2025-09-27 14:13:57 +0800

    add call pearlink test

--- __tests__/pearlink.test.js
+++ __tests__/pearlink.test.js
@@ -1,6 +1,13 @@
 const RawTextDisplayParser = require('..')
 
 describe('Parse PearLink', () => {
+  test('onpearlink callback is triggered for pear:// links', () => {
+    const mockPearLink = jest.fn()
+    const parser = new RawTextDisplayParser({ onpearlink: mockPearLink })
+    parser.appendText('pear://example')
+    expect(mockPearLink).toHaveBeenCalledWith('pear://example')
+  })
+
   test('setPearLink creates pear-link display entry', () => {
     const parser = new RawTextDisplayParser()
     parser.appendText('pear://example')
