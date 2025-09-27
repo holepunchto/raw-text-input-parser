b7097031f5ae986a2a62b0013fc65d88f67c0d4f
Fred <gasolin@gmail.com>
2025-09-27 14:11:09 +0800

    update desc

--- __tests__/pearlink.test.js
+++ __tests__/pearlink.test.js
@@ -1,6 +1,6 @@
 const RawTextDisplayParser = require('..')
 
-describe('Parse Link', () => {
+describe('Parse PearLink', () => {
   test('setPearLink creates pear-link display entry', () => {
     const parser = new RawTextDisplayParser()
     parser.appendText('pear://example')
