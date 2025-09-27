d4982b172bce0296ecc41c5e0b7fd18fd68464cd
Fred <gasolin@gmail.com>
2025-09-27 14:09:06 +0800

    [CI] add jest tests

---
+++ __tests__/pearlink.test.js
@@ -0,0 +1,17 @@
+const RawTextDisplayParser = require('..')
+
+describe('Parse Link', () => {
+  test('setPearLink creates pear-link display entry', () => {
+    const parser = new RawTextDisplayParser()
+    parser.appendText('pear://example')
+    const success = parser.setPearLink('pear://example')
+    expect(success).toBe(true)
+    expect(parser.display).toContainEqual({
+      type: 'pear-link',
+      start: 0,
+      end: 14,
+      content: 'pear://example',
+      link: 'pear://example'
+    })
+  })
+})
